5ddec1a8f912b3737e9ed52c3eb9632a60395635
Artur Safiullin <shard.f@mail.ru>
2021-04-04 18:07:41 +0300

    splited files and added opportunity to launch app from main.py

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -2,47 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 
+try:
+    from app.api.movies import movies
+except ModuleNotFoundError:
+    from api.movies import movies
+    import uvicorn
+
 app = FastAPI()
 
-fake_movie_db = [
-    {
-        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
-        'plot': 'The surviving members of the resistance face the First Order once again.',
-        'genres': ['Action', 'Adventure', 'Fantasy'],
-        'casts': ['Daisy Ridley', 'Adam Driver']
-    }
-]
+app.include_router(movies)
 
-class Movie(BaseModel):
-    name: str
-    plot: str
-    genres: List[str]
-    casts: List[str]
-
-@app.get('/', response_model=List[Movie])
-async def index():
-    return fake_movie_db
-
-@app.post('/', status_code=201)
-async def add_movie(payload: Movie):
-    movie = payload.dict()
-    print(movie)
-    fake_movie_db.append(movie)
-    return {'id': len(fake_movie_db) - 1}
-
-@app.put('/{id}')
-async def update_movie(id: int, payload: Movie):
-    movie = payload.dict()
-    movies_length = len(fake_movie_db)
-    if 0 <= id <= movies_length:
-        fake_movie_db[id] = movie
-        return None
-    raise HTTPException(status_code=404, detail="Movie with given id not found")
-
-@app.delete('/{id}')
-async def delete_movie(id: int):
-    movies_length = len(fake_movie_db)
-    if 0 <= id <= movies_length:
-        del fake_movie_db[id]
-        return None
-    raise HTTPException(status_code=404, detail="Movie with given id not found")
+if __name__ == "__main__":
+    uvicorn.run(app, host = "0.0.0.0", port = 8000)
