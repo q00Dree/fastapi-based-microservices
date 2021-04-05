9440770e9f0675a9c3ff0d4b2aa06d054e0fbae2
Artur Safiullin <shard.f@mail.ru>
2021-04-06 02:55:07 +0300

    fixes in movies service

--- src/movie-services/app/api/services.py
+++ src/movie-services/app/api/services.py
@@ -5,5 +5,5 @@ CAST_SERVICE_HOST_URL = 'http://localhost:8002/api/v1/casts/'
 url = os.environ.get('CAST_SERVICE_HOST_URL') or CAST_SERVICE_HOST_URL
 
 def is_cast_present(cast_id: int):
-    r = httpx.get(f'{url}{cast_id}')
-    return True if r.status_code == 200 else False
+  r = httpx.get(f'{url}{cast_id}')
+  return True if r.status_code == 200 else False
